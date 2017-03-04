6c270ca599faeb2a646d2eec5e07427ce33e6a67
newapple <734719526@qq.com>
2017-03-04 10:46:48 +0800

    loading login

--- app/src/main/java/com/shian/shianlifezx/activity/LoginActivity.java
+++ app/src/main/java/com/shian/shianlifezx/activity/LoginActivity.java
@@ -130,8 +130,10 @@ public class LoginActivity extends BaseActivity {
 	}
 	@OnClick(R.id.btn_login_web)
 	void loginWeb(View v){
-		Intent in=new Intent(this,WebActivity.class);
-		in.putExtra("url","http://m.e-funeral.cn");
+//		Intent in=new Intent(this,WebActivity.class);
+//		in.putExtra("url","http://m.e-funeral.cn");
+//		startActivity(in);
+		Intent in=new Intent(this,LoginPhoneActivity.class);
 		startActivity(in);
 	}
 }
